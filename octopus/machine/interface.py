10445099000d01739d8bcdd63b50d9a51869b1ec
Richard Ingham <mail@richardingham.net>
2014-05-02 18:10:14 +0100

    Some new interface objects

--- octopus/machine/interface.py
+++ octopus/machine/interface.py
@@ -1,5 +1,5 @@
 # System Imports
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 
 # Sibling Imports
 from ..util import Event, now
@@ -9,117 +9,23 @@ __all__ = ["InterfaceSection", "InterfaceSectionSet", "Trace", "Property"]
 
 TRACE_TIME = 60
 
-
-def _prop (p):
-	result = {
-		"name":  p.alias,
-		"title": p.title if hasattr(p, "title") else "",
-		"unit":  p.unit if hasattr(p, "unit") else "",
-		"type":  p.type.__name__,
-		"value": p.value
-	}
-
-	if not p.immutable:
-		result["edit"] = True
-		result["disabled"] = False
-
-		if p.type in (int, float, long):
-			type = "number"
-		elif p.type is boolean:
-			type = "switch"
-		elif p.type is str:
-			type = "string"
-
-		if hasattr(p, "min") and p.min is not None:
-			result["min"] = p.min
-		if hasattr(p, "max") and p.max is not None:
-			result["max"] = p.max
-		if hasattr(p, "options") and p.options is not None:
-			result["options"] = p.options
-			type = "select"
-
-		result["control_type"] = type
-
-	else:
-		result["edit"] = False
-
-	return result
-
-def _img (p):
-	result = {
-		"name":  p.alias,
-		"title": p.title,
-		"value": p.value
-	}
-
-	return result
-
-def _trace (t):
-	if "colours" not in t or len(t["colours"]) < len(t["traces"]):
-		colours = None # generate_colours(len(t["traces"]))
-	else:
-		colours = iter(t["colours"])
-
-	interval = t["maxtime"] if "maxtime" in t else TRACE_TIME
-	start = now() - interval
-	
-	def compress (point):
-		try:
-			return (round(point[0] - start, 1), round(point[1], 2))
-		except TypeError:
-			return 0
-
-	def _trace (t):
-		r = _prop(t)
-		r["zero"] = round(start, 1)
-		r["max"] = round(interval, 1)
-		r["data"] = map(compress, t.get(-interval))
-		if colours is not None:
-			r["colour"] = colours.next()
-
-		return r
-
-	return {
-		"title": t["title"],
-		"streams": [_trace(x) for x in t["traces"]],
-		"unit": t["unit"],
-		"min_display_time": t["mintime"] if "mintime" in t else 60,
-		"max_display_time": interval
-	}
-
-def _control (c):
-	result = {
-		"name":     c.alias,
-		"type":     c.type,
-		"title":    c.title,
-		"value":    c.value,
-		"unit":     c.unit,
-		"variable": c.var_alias
-	}
-
-	for key in ("min", "max", "options"):
-		if hasattr(c, key):
-			result[key] = getattr(c, key)
-
-	return result
+_alias_counters = defaultdict(lambda: 0)
+def _alias (name):
+	_alias_counters[name] += 1
+	return "{:s}{:d}".format(name, _alias_counters[name])
 
 class InterfaceSection (object):
-	def __init__ (self, properties = None, traces = None, controls = None, title = "", name = None):
+	def __init__ (self, items = None, title = "", name = None):
 		self.name = name or title
 		self.title = title
-		self.properties = properties or []
-		self.controls = controls or []
-		self.traces = OrderedDict()
+		self.items = items or []
 
-		if traces is not None:
-			for t in traces:
-				try:
-					self.traces[t["name"]] = t
-				except KeyError:
-					self.traces[t["title"]] = t
+		self.map = {}
+		for i in items:
+			self.map[i.name] = i
 
 	def __getitem__ (self, key):
-		if key in ("name", "title", "properties", "controls", "traces"):
+		if key in ("name", "title", "properties", "traces"):
 			return getattr(self, key)
 		else:
 			raise KeyError
@@ -128,16 +34,11 @@ class InterfaceSection (object):
 		return {
 			"name": self.name,
 			"title": self.title,
-			"traces": [_trace(t) for t in self.traces.itervalues()],
-			"properties": [_prop(p) for p in self.properties if p.type not in ("Image",)],
-			"images": [_img(p) for p in self.properties if p.type == "Image"],
-			"controls": [_control(c) for c in self.controls]
+			"items": [i.serialize() for i in self.items]
 		}
 
 
 class InterfaceSectionSet (OrderedDict):
-
-	trace_time = 30
 
 	event = Event()
 	
@@ -156,10 +57,6 @@ class InterfaceSectionSet (OrderedDict):
 		section.name = name
 		OrderedDict.__setitem__(self, name, section)
 
-		for c in section.controls:
-			self.controls[c.alias] = c
-			c.event += self.event
-
 		for p in section.properties:
 			self.properties[p.alias] = p
 
@@ -173,10 +70,6 @@ class InterfaceSectionSet (OrderedDict):
 
 	def _delitem (self, name):
 		section = self[name]
-
-		for c in section.controls:
-			del self.controls[p.alias]
-			c.event -= self.event
 
 		for p in section.properties:
 			del self.properties[p.alias]
@@ -200,6 +93,7 @@ def fromDict (dict):
 	section = InterfaceSection()
 	try:
 		for trace in dict["traces"]:
+			Trace(trace)
 			
 	except (KeyError, TypeError):
 		pass
@@ -207,9 +101,139 @@ def fromDict (dict):
 	
 
 class Trace (object):
-	pass
+	title = None
+	colours = None
+	variables = None
+	mintime = 60
+	maxtime = TRACE_TIME
+
+	def __init__ (self, **kwargs):
+		self.name = _alias("t")
+
+		for key, value in kwargs.iteritems():
+			if hasattr(self, key):
+				setattr(self, key, value)
+
+	def serialize (self):
+		interval = self.maxtime
+		start = now() - interval
+		colours = iter(self.colours) if self.colours is not None else None
+		
+		def compress (point):
+			try:
+				return (round(point[0] - start, 1), round(point[1], 2))
+			except TypeError:
+				return 0
+
+		def variable (t):
+			r = {
+				"name":  t.alias,
+				"title": t.title if hasattr(t, "title") else "",
+				"unit":  t.unit if hasattr(t, "unit") else "",
+				"zero": round(start, 1),
+				"max": round(interval, 1),
+				"data": map(compress, t.get(-interval))
+			}
+
+			if colours is not None:
+				r["colour"] = colours.next()
+
+			return r
+
+		return {
+			"name": self.name,
+			"title": self.title,
+			"streams": [variable(v) for v in self.variables],
+			"min_display_time": self.mintime,
+			"max_display_time": self.maxtime
+		}
 
 
 class Property (object):
-	pass
+	def __init__ (self, variable):
+		self.name = variable.alias
+		self.variable = variable
 
+	def serialize (self):
+		p = self.variable
+
+		result = {
+			"name":  p.alias,
+			"title": p.title if hasattr(p, "title") else "",
+			"unit":  p.unit if hasattr(p, "unit") else "",
+			"type":  p.type.__name__,
+			"value": p.value
+		}
+
+		if not p.immutable:
+			result["edit"] = True
+			result["disabled"] = False
+
+			if p.type in (int, float, long):
+				type = "number"
+			elif p.type is boolean:
+				type = "switch"
+			elif p.type is str:
+				type = "string"
+
+			if hasattr(p, "min") and p.min is not None:
+				result["min"] = p.min
+			if hasattr(p, "max") and p.max is not None:
+				result["max"] = p.max
+			if hasattr(p, "options") and p.options is not None:
+				result["options"] = p.options
+				type = "select"
+
+			result["control_type"] = type
+
+		else:
+			result["edit"] = False
+
+		return result
+
+class Image (Property):
+	def serialize (self):
+		p = self.variable
+		return {
+			"name":  p.alias,
+			"title": p.title,
+			"value": p.value,
+			"type":  "image",
+			"edit":  False
+		}
+
+class Button (object):
+	"""A push button control (not associated with a variable)."""
+
+	type = "button"
+
+	action = None
+	"""A function to perform when the button is pressed."""
+
+	args = []
+	"""Arguments to be passed to the action function when it is called."""
+
+	kwargs = {}
+	"""Keywords to be passed to the action function when it is called."""
+
+	def __init__ (self, title, action = None, *args, **kwargs):
+		self.name = _alias("b")
+
+		self.title = title
+		self.action = action
+		self.args = args
+		self.kwargs = kwargs
+
+	def serialize (self):
+		return {
+			"name": self.name,
+			"title": self.title,
+			"disabled": False
+		}
+
+	def update (self, value):
+		try:
+			if not self._disabled:
+				return self.action(*self.args, **self.kwargs)
+		except TypeError:
+			pass
