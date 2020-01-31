b4f4d6841fec4fb2b1fba4bf3ec07726aa9a9a6c
Richard Ingham <mail@richardingham.net>
2020-01-31 08:24:14 +0000

    Remove legacy server.

--- twisted/plugins/octopus_plugin.py
+++
@@ -1,8 +0,0 @@
-from twisted.application.service import ServiceMaker
-
-octopus = ServiceMaker(
-    name = 'octopus', 
-	module = 'octopus.server.tap', 
-	description = 'Run the octopus service.', 
-	tapname = 'octopus'
-)
